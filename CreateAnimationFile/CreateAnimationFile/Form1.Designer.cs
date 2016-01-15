71def6bdf6b1729ca9591a85c8ee5cf63f1a257d
gkocevar1 <kocevar@hotmail.com>
2016-01-15 21:29:06 +0100

    Added animation generation project

---
+++ CreateAnimationFile/CreateAnimationFile/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CreateAnimationFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.textBoxFrames = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxXML = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source folder";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Enabled = false;
+            this.textBoxSource.Location = new System.Drawing.Point(200, 10);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(572, 20);
+            this.textBoxSource.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(16, 133);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(299, 416);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File name";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(119, 69);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(210, 20);
+            this.textBoxFileName.TabIndex = 6;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(335, 67);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 7;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(119, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Select folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Destination folder";
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Enabled = false;
+            this.textBoxDestination.Location = new System.Drawing.Point(200, 41);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(572, 20);
+            this.textBoxDestination.TabIndex = 10;
+            // 
+            // textBoxFrames
+            // 
+            this.textBoxFrames.Location = new System.Drawing.Point(119, 104);
+            this.textBoxFrames.Name = "textBoxFrames";
+            this.textBoxFrames.Size = new System.Drawing.Size(75, 20);
+            this.textBoxFrames.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Frames per seconds";
+            // 
+            // textBoxXML
+            // 
+            this.textBoxXML.Location = new System.Drawing.Point(335, 133);
+            this.textBoxXML.Multiline = true;
+            this.textBoxXML.Name = "textBoxXML";
+            this.textBoxXML.Size = new System.Drawing.Size(437, 416);
+            this.textBoxXML.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.textBoxXML);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFrames);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.TextBox textBoxFrames;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxXML;
+    }
+}
+
---
+++ CreateAnimationFile/CreateAnimationFile/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CreateAnimationFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var source = @"C:\Work\Samples\AndroidSamples\trunk\App1\App1\Resources\drawable\01_kiss_1";
+            textBoxSource.Text = source;
+            textBoxDestination.Text = @"C:\Work\Samples\AndroidSamples\trunk\App1\App1\Resources\anim";
+            textBoxFrames.Text = "15";
+            textBoxFileName.Text = source.Substring(source.LastIndexOf("\\") + 1);
+
+            InitializeListView();
+
+            FillListView(textBoxSource.Text);
+        }
+
+        private void InitializeListView()
+        {
+            listView1.View = View.Details;
+            listView1.CheckBoxes = true;
+
+            listView1.Columns.Add("", 30);
+            listView1.Columns.Add("Name", 200);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = false;
+            dialog.SelectedPath = textBoxSource.Text;
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxSource.Text = dialog.SelectedPath;
+                textBoxFileName.Text = textBoxSource.Text.Substring(textBoxSource.Text.LastIndexOf("\\") + 1);
+                textBoxXML.Text = string.Empty;
+                FillListView(textBoxSource.Text);
+            }
+        }
+
+        private void FillListView(string folder)
+        {
+            
+            listView1.Items.Clear();
+
+            var files = Directory.GetFiles(folder);
+
+            foreach (var file in files)
+            {
+                var f = new FileInfo(file);
+
+                var item = new ListViewItem("");
+                item.Checked = true;
+                item.SubItems.Add(f.Name);
+
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = false;
+            dialog.SelectedPath = textBoxDestination.Text;
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxDestination.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            var duration = (int)(1000 / int.Parse(textBoxFrames.Text));
+
+            var sb = new StringBuilder();
+            sb.AppendLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            sb.AppendLine(@"<animation-list xmlns:android=""http://schemas.android.com/apk/res/android"" android:oneshot=""true"">");
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.Checked)
+                {
+                    var name = item.SubItems[1].Text;
+                    sb.AppendLine(string.Format(@"<item android:drawable=""@drawable/{0}"" android:duration=""{1}"" />", name.Substring(0, name.IndexOf(".")), duration));
+                }
+            }
+
+            sb.AppendLine(@"</animation-list>");
+
+            textBoxXML.Text = sb.ToString();
+
+            // save to file
+            File.WriteAllText(Path.Combine(textBoxDestination.Text, textBoxFileName.Text + ".xml"), sb.ToString());
+        }
+    }
+}
